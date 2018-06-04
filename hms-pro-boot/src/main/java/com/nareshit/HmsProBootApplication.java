046238ad8203c2bf379940c88fd717cd86bd099b
raviks1994 <kravisingh01@gmail.com>
2018-06-04 10:36:30 +0530

    temperory changes to HmsProBootApplication.java

--- hms-pro-boot/src/main/java/com/nareshit/HmsProBootApplication.java
+++ hms-pro-boot/src/main/java/com/nareshit/HmsProBootApplication.java
@@ -17,7 +17,7 @@ import com.zaxxer.hikari.HikariDataSource;
 public class HmsProBootApplication implements CommandLineRunner{
 	
 	@Autowired
-	private DataSource ds;
+	private DataSource ds; //data source object creation
 	
 	@Autowired
 	private HikariDataSource hds;
